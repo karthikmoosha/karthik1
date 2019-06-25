4a3234ef2cb1e276910e45465ed341add7bba767
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-06-25 12:01:20 +0530

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,5 +1,5 @@
 ch=input()
-if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+if(ch=='A' or ch=='a' or ch=='U' or ch=='u' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='E' or ch=='e'):
   print("Vowel")
 else:
     print("Consonant")
