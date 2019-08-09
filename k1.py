8509b18df305296f29d62f70e33efdcce8b40cfb
karthikmoosha <51981064+karthikmoosha@users.noreply.github.com>
2019-08-09 15:02:00 +0530

    Create k1.py

---
+++ k1.py
@@ -0,0 +1,18 @@
+import sys, string, math, itertools
+
+n1 = input()
+n1 = int(n1)
+L = []
+
+for i in range(0,n1) :
+    s = input()
+    L.append(s)
+
+common_prefix = []
+for i in zip(*L):
+    if i.count(i[0]) == len(i):
+        common_prefix.append(i[0])
+    else:
+        break
+ans = ''.join(common_prefix)
+print(ans)
